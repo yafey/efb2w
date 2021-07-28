000df25578250c5a0c123b24f5784aec553b3a24
yafey <hyf.position@gmail.com>
2021-07-28 17:40:01 +0000

    add comment r10-boot-timeout

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# to avoid [Heroku Error Codes | Heroku Dev Center](https://devcenter.heroku.com/articles/error-codes#r10-boot-timeout)
 python3 -m http.server $PORT -d  /root/html & 
 #python3 /root/.ehforwarderbot/server.py &
 ehforwarderbot -p default
